41483ff5d7702745f8352479bc43d27df0624786
Charlie Chrisman <charlie.chrisman@gmail.com>
2019-04-01 15:46:11 -0700

    update functions

--- functions/webhook-listener.js
+++ functions/webhook-listener.js
@@ -1,30 +1,29 @@
-const contentful = require("contentful");
+import contentful from "contentful"
+const { SPACE_ID, ENVIRONMENT_ID, DELIVERY_TOKEN } = process.env
 
 const client = contentful.createClient({
-  space: process.env.SPACE_ID,
-  environment: process.env.ENVIRONMENT_ID, // defaults to 'master' if not set
-  accessToken: process.env.DELIVERY_TOKEN
-});
+  space: SPACE_ID,
+  environment: ENVIRONMENT_ID, // defaults to 'master' if not set
+  accessToken: DELIVERY_TOKEN
+})
 
-console.log("process.env.SPACE_ID: ", process.env.SPACE_ID);
-console.log("process.env.ENVIRONMENT_ID: ", process.env.ENVIRONMENT_ID);
-console.log("process.env.DELIVERY_TOKEN: ", process.env.DELIVERY_TOKEN);
+console.log("SPACE_ID: ", SPACE_ID)
+console.log("ENVIRONMENT_ID: ", ENVIRONMENT_ID)
+console.log("DELIVERY_TOKEN: ", DELIVERY_TOKEN)
 
 exports.handler = function (event, context, callback) {
   console.log("event: ", JSON.stringify(event, null, 2));
   const content_type_id = event.body.fields.variations["en-US"][0].sys.contentType.sys.id;
   const entry_ids = event.body.fields.variations["en-US"].reduce((acc, cur) => acc.concat(cur.sys.id),[]);
   console.log("entry_ids:", entry_ids);
-  client
-    .getEntries({
-      content_type: content_type_id,
-      "sys.id[in]": entry_ids.join(",")
+  client.getEntries({
+    content_type: content_type_id,
+    "sys.id[in]": entry_ids.join(",")
+  }).then(entries => {
+    console.log("entries:", JSON.stringify(entries, null, 2));
+    callback(null, {
+      statusCode: 200,
+      body: `Hello, World\nhere are your entries: \n${JSON.stringify(entries,null,2)}`
     })
-    .then(entries => {
-      console.log("entries:", JSON.stringify(entries, null, 2));
-      callback(null, {
-        statusCode: 200,
-        body: `Hello, World\nhere are your entries: \n${JSON.stringify(entries,null,2)}`
-      });
-    });
-};
+  })
+}
